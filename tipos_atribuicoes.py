0d7804a61866f59cceada515291d1817c0c65ff8
saulosgil <saulosgil@hotmail.com>
2023-08-28 17:49:06 -0300

    tipos de instruções script

---
+++ tipos_atribuicoes.py
@@ -0,0 +1,28 @@
+"""
+Tipos de instruções
+O interpretador Python entende intruções que estão divididas em 3 categorias
+
+Expressões (expressions)
+Toda instrução que gera um processamento e espera um valor de retorno, exprimimos qual operação desejamos que seja computada usando funções ou operadores matemáticos e sempre esperamos obter um resultado para armazenar em variáveis ou efetuar operações de comparação.
+
+exemplo:
+"""
+# Expressão literal, retorno o próprio número
+1
+
+# Expressão, retorna o próprio texto
+'Bruno'
+
+# Expressão de chamada de método ou função
+"Bruno".upper()
+
+
+# Expressão com operador, retorna um resultado booleano
+1 > 2
+89 >= 89
+
+# com strings
+"Bruno" != "Joao"
+
+# Chamada de função
+sum([1,2,3])
